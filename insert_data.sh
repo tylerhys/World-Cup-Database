f6cebf829419d0230ffc2730f2ed7d0df55ac769
tylerhys <73792788+tylerhys@users.noreply.github.com>
2023-04-04 21:58:15 +0800

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -1,41 +1,57 @@
 #! /bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+clear=$($PSQL "truncate games,teams")
+echo -e "Tables Games and Teams have been cleared.\n"
 
-echo -e "\nTotal number of goals in all games from winning teams:"
-echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
+cat games.csv | while IFS="," read year round winner opponent winner_goals opponent_goals
+do
+  # Insert winner into column name in table Teams
+  if [[ $winner != "winner" ]]
+    then
+    winner_id=$($PSQL "SELECT team_id from teams where name = '$winner'")
+    if [[ -z $winner_id ]]
+      then
+      team=$($PSQL "INSERT INTO teams(name) values('$winner')")
+      if [[ $team == 'INSERT 0 1' ]]
+        then
+        echo $winner has been added into teams.
+        winner_id=$($PSQL "SELECT team_id from teams where name = '$winner'")
+      fi
+    fi
+  fi    
 
-echo -e "\nTotal number of goals in all games from both teams combined:"
-echo "$($PSQL "SELECT SUM(winner_goals+opponent_goals) from games")"
+  # Insert opponent into column name in table Teams
+  if [[ $opponent != "opponent" ]]
+    then
+    opponent_id=$($PSQL "SELECT team_id from teams where name = '$opponent'")
+    if [[ -z $opponent_id ]]
+      then
+      team=$($PSQL "INSERT INTO teams(name) values('$opponent')")
+      if [[ $team == 'INSERT 0 1' ]]
+        then
+        echo $opponent has been added into teams.
+        opponent_id=$($PSQL "SELECT team_id from teams where name = '$opponent'")
+      fi
+    fi
+  fi    
 
-echo -e "\nAverage number of goals in all games from the winning teams:"
-echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
+  if [[ $year != 'year' && $round  != 'round' ]]
+  then
+  entry=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals)
+   values('$year','$round','$winner_id','$opponent_id','$winner_goals','$opponent_goals')")
+    if [[ $entry = 'INSERT 0 1' ]]
+    then
+    echo $year,$round,$winner_id,$opponent_id,$winner_goals,$opponent_goals has been inserted into games.
+    fi
+  fi 
+done
 
-echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
-echo "$($PSQL "SELECT ROUND(AVG(winner_goals),2) FROM games")"
-
-echo -e "\nAverage number of goals in all games from both teams:"
-echo "$($PSQL "SELECT AVG(winner_goals+opponent_goals) from games")"
-
-echo -e "\nMost goals scored in a single game by one team:"
-echo "$($PSQL "SELECT winner_goals from games order by winner_goals desc limit 1")"
-
-echo -e "\nNumber of games where the winning team scored more than two goals:"
-echo "$($PSQL "select count(*) from games where winner_goals > 2")"
-
-echo -e "\nWinner of the 2018 tournament team name:"
-echo "$($PSQL "select b.name from games a left join teams b on a.winner_id = b.team_id where a.round = 'Final' and a.year = 2018")"
-
-echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo "$($PSQL "select b.name from games a left join teams b on a.winner_id = b.team_id or a.opponent_id = b.team_id where a.round = 'Eighth-Final' and a.year = 2014 order by name;")"
-
-echo -e "\nList of unique winning team names in the whole data set:"
-echo "$($PSQL "select distinct b.name from games a left join teams b on a.winner_id = b.team_id order by b.name;")"
-
-echo -e "\nYear and team name of all the champions:"
-echo "$($PSQL "select a.year,b.name from games a left join teams b on a.winner_id = b.team_id where round = 'Final' order by a.year;")"
-
-echo -e "\nList of teams that start with 'Co':"
-echo "$($PSQL "select name from teams where name like 'Co%'")"
+echo -e "\nJob is Completed."
